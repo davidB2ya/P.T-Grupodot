b09a28d82eb72281034dd7c6026923229b9b3f07
davidB2ya <dbedoya0408@gmail.com>
2022-02-03 22:56:07 -0500

    settings in boxes the benefits

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const btn_menu_close = document.getElementById('btn-menu-close');
 
 const nav = document.getElementById('nav-menu');
 
-btn_menu.addEventListener('click', () =>{
+btn_menu.addEventListener('click', () => {
     nav.classList.add('visible')
 })
-btn_menu_close.addEventListener('click', () =>{
+btn_menu_close.addEventListener('click', () => {
     nav.classList.remove('visible')
 })
